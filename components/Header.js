bce119e424311aa3b45f3c72274ef8187b407f1c
Andy <olshansa@gmail.com>
2023-03-23 20:35:25 -0400

    lets get started

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from './Image'
+
+function Header() {
+  return (
+    <Image />
+  )
+}
+
+export default Header
---
+++ components/Image.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Image() {
+    return (
+        <>
+            <div>Image should be below this</div>
+            <img src="pic.png" alt="Click to add photo"/>
+        </>
+    )
+}
+
+export default Image
